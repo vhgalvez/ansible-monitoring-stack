55e92ebac8c92acc06e272320493a9567c4333da
Victor Galvez <vhgalvez@gmail.com>
2025-04-17 21:35:32 +0200

    update

--- sh-forward/start-monitoring.sh
+++ sh-forward/start-monitoring.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 echo "🔄 Iniciando port-forward para Grafana..."
-nohup kubectl port-forward -n monitoring svc/grafana 3000:80 > grafana.log 2>&1 &
+
+sudo env "PATH=$PATH" KUBECONFIG=$HOME/.kube/config nohup kubectl port-forward -n monitoring svc/grafana --address 0.0.0.0 3001:80 > ~/grafana.log 2>&1 &
 
 echo "🔄 Iniciando port-forward para Prometheus..."
-nohup kubectl port-forward -n monitoring svc/prometheus-server 9090:80 > prometheus.log 2>&1 &
+
+sudo env "PATH=$PATH" KUBECONFIG=$HOME/.kube/config nohup kubectl port-forward -n monitoring svc/prometheus-server --address 0.0.0.0 9091:80 > ~/prometheus.log 2>&1 &
 
 echo "✅ Port-forward en segundo plano. Verifica con: ps aux | grep port-forward"
+
+
+
+
+
+
