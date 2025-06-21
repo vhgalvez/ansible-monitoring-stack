75b96f9e86dc8ed8fab9fbeb0de763ca7f028a29
Victor Galvez <vhgalvez@gmail.com>
2025-06-21 16:49:36 +0200

    update

--- sh-forward/stop-monitoring.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🛑 Deteniendo port-forward de Grafana..."
-pkill -f "kubectl port-forward -n monitoring svc/grafana"
-
-echo "🛑 Deteniendo port-forward de Prometheus..."
-pkill -f "kubectl port-forward -n monitoring svc/prometheus-server"
-
-echo "✅ Todos los procesos de port-forward han sido detenidos."
